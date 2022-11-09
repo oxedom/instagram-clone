18bfbab427f55cedc1b9bcd964243f89211a7f7f
=oxedom <oxedom@gmail.com>
2022-11-09 20:08:49 +0200

    fetching dataworks + added tailwind basics

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {firestore} from './firebase'
 import { collection, getDocs } from "firebase/firestore"; 
 
 function App() {
 
-  const handleLihi = async () => {
-
+  const getUsers = async () => {
+    let users = []
     const querySnapshot = await getDocs(collection(firestore, "users"));
-
     querySnapshot.forEach((doc) => {
-    console.log(doc.data())
+    users.push({...doc.data(), id: doc.id})
 
     });
-
+    setUsers(users)
   }
+
+  const [users, setUsers] = useState([])
+
 
   useEffect( ()=> {
 
-  handleLihi()
+    getUsers()
+   
 
   }, [])
 
   return (
     <div className="App">
-      <h1> Hello Firebase</h1>
+      {users.map((user,i) => <h1 className="text-3xl font-bold underline" key={user.id}> {user.username} </h1>)}
+      <h1> Hello </h1>
     </div>
   );
 }
