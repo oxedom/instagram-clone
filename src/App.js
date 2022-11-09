0861951bbaea9b9676b8fbad8c47f4bcc80a828f
=oxedom <oxedom@gmail.com>
2022-11-09 18:15:54 +0200

    Firebase Query works'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { useEffect } from 'react';
+import {firestore} from './firebase'
+import { collection, getDocs } from "firebase/firestore"; 
+
 function App() {
+
+  const handleLihi = async () => {
+
+    const querySnapshot = await getDocs(collection(firestore, "users"));
+
+    querySnapshot.forEach((doc) => {
+    console.log(doc.data())
+
+    });
+
+  }
+
+  useEffect( ()=> {
+
+  handleLihi()
+
+  }, [])
+
   return (
     <div className="App">
-
+      <h1> Hello Firebase</h1>
     </div>
   );
 }
