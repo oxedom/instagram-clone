71af6eb0cdd2de3f61d50455c1da74e8cee26c47
=oxedom <oxedom@gmail.com>
2022-11-14 14:04:00 +0200

    Made pretty login

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,12 +1,19 @@
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { Link } from "react-router-dom";
 import { auth} from '../../firebase'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import img from '../../assests/text-logo.png'
 const Signin = () => {
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    const [buttonState, setButtonState] = useState()
+
+    useEffect(() => {
+    if(password.length < 6) { setButtonState('bg-blue-300')}
+    else { setButtonState('bg-blue-500')}
+    }, [email,password])
 
     function handleSubmit(e) {
         e.preventDefault()
@@ -24,18 +31,19 @@ const Signin = () => {
 
     return ( <div className="bg-slate-100 p-10">
         
-        <div className="flex mx-auto flex-col p-4 max-w-2xl gap-3 mt-10"> 
+        <div className="flex mx-auto flex-col p-10 max-w-2xl gap-5 mt-10  bg-white"> 
+        <img src={img} className="object-scale-down"/>
+        <form className="flex flex-col gap-3 p-1" onSubmit={handleSubmit}>
+              
 
-
+        <input className="bg-slate-50 p-2" placeholder='email'  id='email' type="email" onChange={(e) => {setEmail(e.target.value)} } name="email"></input>
+        <input className="bg-slate-50 p-2" placeholder='password' id='password' min="6" type="password" onChange={(e) => {setPassword(e.target.value)} } name="password"></input>
+        <button className={`rounded text-white font-bold btn p-2 ${buttonState} hover:bg-blue-700" type="submit`}> Sign in  </button>
+        </form>
+        
 
         </div>
-        <form  className='flex mx-auto flex-col p-4 max-w-2xl gap-3 mt-10'  onSubmit={handleSubmit}>
-        
-        <img src={img} className="object-scale-down"/>
-        <input id='email' type="email" onChange={(e) => {setEmail(e.target.value)} } name="email"></input>
-        <input id='password' min="6" type="password" onChange={(e) => {setPassword(e.target.value)} } name="password"></input>
-        <button className="rounded-full text-white font-bold btn p-2 bg-blue-500 hover:bg-blue-700" type="submit"> Sign in  </button>
-        </form>
+ 
     </div>  );
 }
  
