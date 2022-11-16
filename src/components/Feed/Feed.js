9a8d8d9be3bb50557af6f26523e103bd31f86a4e
=oxedom <oxedom@gmail.com>
2022-11-16 19:59:47 +0200

    removed navigte function from feed

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -4,10 +4,7 @@ const Feed = () => {
 
   const navigate = useNavigate()
   
-  const handleClick = () => {
-    navigate('/home')
-
-  }
+ 
   return (
     <div className="bg-slate-50  flex-grow flex justify-center">
       <h1 onClick={handleClick}> Hello from Feed</h1>
