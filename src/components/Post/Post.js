acbf1acb51bf1606daa3e4283cc217dbdda386b0
=oxedom <oxedom@gmail.com>
2022-11-17 14:23:20 +0200

    post container updating

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -3,17 +3,27 @@ import PostButtons from "../PostButtons/PostButtons";
 const Post = (props) => {
 
     const {url, text, uid, likes} = props.props
-
+    const temp = 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'
     const fetchUser = (id) => 
     {
 
     } 
-    return ( <div className="bg-blue" >
-   
-        <img className="" alt={text }src={url} /> 
-        <PostButtons props={{likes, uid}}> </PostButtons>
+    return ( <div className="flex flex-col shadow-md border-solid mt-3 ">
         
-    </div>  );
+        <div className="bg-white p-4 rounded-lg"> 
+ 
+        <div className="flex">
+   
+        <img className="rounded-full aspect-square w-20 h-20 " alt='profile picture of user' src={temp} />
+        <p className="self-end"> {uid} </p> 
+        </div>
+    
+    
+        </div>
+        <img className="object-fit w-96 md:w-96 " alt={text }src={url} /> 
+        <PostButtons props={{likes, uid, text}}> </PostButtons>
+        
+    </div >  );
 }
  
 export default Post;
--- src/components/PostButtons/PostButtons.js
+++ src/components/PostButtons/PostButtons.js
@@ -1,13 +1,44 @@
+import unlike from '../../assests/heart.png'
+import liked from '../../assests/darkheart.png' 
+import commment from  '../../assests/comment.png' 
+
+import { useEffect, useState } from 'react'
+
 const PostButtons = (props) => {
+
+
+    const [likedState, setLikedState] = useState(unlike)
+
+    const handleLike = () => 
+    {
+        if(likedState === liked) 
+        { setLikedState(unlike)}
+        else { setLikedState(liked)}
+    }
+    useEffect(() => 
+    {
+        if(true) { setLikedState(liked)}
+    }, [])
     const {url, text, uid, likes} = props.props
 
 
-    return (         <div>
-        <ul className="flex gap-2">
-            <li>Heart</li>
-            <li>Comments</li>
+    return (<div className='bg-white '>
+
+        <div className='p-2 flex flex-col gap-3 '>
+
+        <ul className="flex gap-4 object-contain w-20 mt-2">
+            <li> <img onClick={handleLike} src={likedState} /> </li>
+            <li> <img src={commment}/> </li>
         </ul>
         <p> {likes} likes </p>
+        <div className='flex gap-2'>
+
+        <p className='font-semibold '> {uid} </p> <span> {text} </span>
+        </div>
+
+
+        </div>
+
     </div> );
 }
  
