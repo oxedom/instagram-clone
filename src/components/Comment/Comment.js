be6f3857d68dc6100fcf9b401ab5ec5ddf8fbd0a
=oxedom <oxedom@gmail.com>
2022-11-29 10:53:31 +0200

    added comment section comp to minimze size of postButtons comp, refactored liking logic, added showmore for comment and delete unused vars

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,23 +1,21 @@
-import { useEffect, useState } from "react";
+
+import ShowMoreText from "react-show-more-text";
+
 import { Link } from "react-router-dom";
 const Comment = (props) => {
   const { text, username, date } = props.commentData;
 
-  const [overFlow, setOverflow] = useState("ml-2 gap-1");
-  useEffect(() => {
-    if (text.length > 40) {
-      setOverflow("ml-2   flex-col");
-    }
-  }, []);
+
 
   return (
-    <div className={`ml-2 flex ${overFlow}`}>
+    <div className={`flex  gap-1`}>
       <Link to={`/profile/${username}`}>
         <span className="font-semibold"> {username} </span>
       </Link>
 
-      <div className="flex-grow-1 flex break-all">
-        <p> {text} </p>
+      <div className="">
+        <ShowMoreText more='show more' less='show less' className='' anchorClass="hover:underline text-gray-500 " width={300} lines={1}> {text} </ShowMoreText>
+
       </div>
     </div>
   );
--- src/components/PostButtons/PostButtons.js
+++ src/components/PostButtons/PostButtons.js
@@ -1,78 +1,60 @@
 import unlike from "../../assests/heart.png";
 import liked from "../../assests/darkheart.png";
 import commmentIcon from "../../assests/comment.png";
-import Comment from "../Comment/Comment";
+
 import { useEffect, useState, useRef} from "react";
 import { PostService } from "../../services/PostService";
 import { Link } from "react-router-dom";
-import { formatDistance } from "date-fns";
+
+import CommentSection from "../CommentSection/CommentSection.js";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../firebase";
+import { set } from "date-fns";
 
 const PostButtons = (props) => {
+
+
+
+
   const { text, likes, username, date, id, comments } = props.postData;
-  const [amountOfLikes, setAmountOfLikes] = useState(1)
-  const [localComments, setLocalComments] = useState([]);
+  const [amountOfLikes, setAmountOfLikes] = useState([])
+
   const postApi = PostService();
   const [likedState, setLikedState] = useState(false);
-  const [textColor, setTextColor] = useState("text-blue-200");
 
-  const [formatedDate, setFormated] = useState("");
-  const [commentText, setComment] = useState("");
-  const inputRefContainer = useRef(null)
 
-  const handleCommentSubmit = (e) => {
-    e.preventDefault();
-    onAuthStateChanged(auth, (user) => {
-      postApi.addComment(id, commentText);
-      setLocalComments((prev) => [
-        ...prev,
-        { text: commentText, username: user.displayName},
-      ]);
-    })
-    setComment("");
-  };
 
-  const handleCommentButton = () => {inputRefContainer.current.focus()}
+
+
 
   const handleLike = () => {
   
     postApi.tooglelikePost(id);
-    if(likedState) { setLikedState(false)}
-    else { setLikedState(true)}
+    if(likedState) { 
+      setLikedState(false)
+      amountOfLikes.pop('fakeLike')
+      
+    }
+    else {
+      amountOfLikes.push('fakeLike')
+      setLikedState(true)}
   };
 
-  useEffect(() => {
-    if (commentText.length > 1) {
-      setTextColor("text-blue-400");
-    } else {
-      setTextColor("text-blue-200");
-    }
-  }, [commentText]);
-
-
-
 
 
   useEffect(() => {
-    setAmountOfLikes(likes.length)
-    setLocalComments(comments);
+    setAmountOfLikes([...likes])
     onAuthStateChanged(auth, (user) => 
     {
-      if (likes.some((l) => l.uid === user.uid)) {setLikedState(true);}
+      if (likes.some((l) => l.uid === user.uid)) {
+        setLikedState(true);}
     })
   
   }, [likes]);
 
-  useEffect(() => {
-    const today = new Date();
-    const dateInWords = formatDistance(date, today.getTime());
-
-    setFormated(dateInWords);
-  }, [date]);
 
   return (
-    <div className="bg-white flex flex-col overflow-hidden  ">
+    <div className="bg-white flex flex-col overflow-hidden   ">
       <div className="p-2 flex flex-col ">
         <ul className="flex gap-4 object-contain w-14 mt-2 ">
           <li>
@@ -86,14 +68,15 @@ const PostButtons = (props) => {
           </li>
           <li>
             {" "}
-            <img onClick={handleCommentButton}
+            <img 
               alt="comment"
               className="hover:cursor-pointer"
               src={commmentIcon}
             />{" "}
           </li>
         </ul>
-        {<p> TOo many likes </p>}
+        {<p>
+        {amountOfLikes.length} likes </p>}
 
         <div className="flex gap-1">
           <Link to={`/profile/${username}`}>
@@ -102,42 +85,13 @@ const PostButtons = (props) => {
 
           <p className=""> {text} </p>
         </div>
-        <div>
-          <p className="text-sm text-slate-400 p-0.5"> {formatedDate} ago </p>
-        </div>
-      </div>
-      <div className="mb-2">
-        {localComments.map((c) => {
-          return (
-            <Comment key={c.date} commentData={c}>
-              {" "}
-            </Comment>
-          );
-        })}
+
+
+
+        <CommentSection  postData={{text, likes, username, date, id, comments,  }}> </CommentSection>
       </div>
 
-      <hr></hr>
-      <form onSubmit={handleCommentSubmit} className="rounded flex">
-        <input
-          ref={inputRefContainer}
-          maxLength={250}
-          className="p-3 flex-grow"
-          value={commentText}
-          onChange={(e) => {
-            setComment(e.target.value);
-          }}
-          type="text"
-          placeholder="Add a comment"
-        ></input>
-        <button
-          alt="submit"
-          type="submit"
-          className={"btn rounded font-bold bg-white m-1 p-1 " + textColor}
-        >
-          {" "}
-          Post{" "}
-        </button>
-      </form>
+      
     </div>
   );
 };
