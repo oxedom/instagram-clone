410ae3810c8199dbe8e0774483e5a7ebfff3bd97
=oxedom <oxedom@gmail.com>
2022-12-04 14:46:23 +0200

    Profile skeleton updates

--- src/components/Skeletons/ProfileSkeleton.js
+++ src/components/Skeletons/ProfileSkeleton.js
@@ -1,14 +1,29 @@
 const ProfileSkeleton = () => {
+
+    const SkeletonSquare = () => {
+        return (         <div className=" bg-gray-200 animate-pulse 
+        h-[130px] w-[130px]
+        sm:w-[200px] sm:h-[200px] 
+        md:w-[250px] md:h-[250px]
+        lg:w-[320px] lg:h-[320px]"> </div>
+);
+    }
+     
+
     return ( 
-    <div className="flex flex-col mt-3">
-    <header className="m-4 flex justify-around ">
+    <div className="flex flex-col mt-8 flex-grow-1">
+    <header className="m-4 flex justify-around  ">
+    
 
     <div className="bg-gray-200  animate-pulse rounded-full object-cover aspect-ratio: auto; w-32 h-32" > </div>    
 
      <div className="flex gap-2 flex-col flex-grow-1  animate-pulse items-center justify-self-center">
-     <p className="h-3 w-20 bg-gray-100">  </p>
+
+        
+     <p className="h-4 w-24 bg-gray-100">  </p>
      <div class="button flex justify-center p-0.5 w-24 h-8 animate-pulse bg-gray-100 rounded">
 
+   
      </div>
 
      
@@ -16,7 +31,12 @@ const ProfileSkeleton = () => {
 
      </header>
 
-     <div className="m-5 flex flex-col gap-2  ">
+
+     <div>
+     <p className="ml-16 h-2 w-32 rounded-md bg-gray-50">  </p>
+     </div>
+
+     <div className="m-2 flex flex-col gap-2 w-max ">
  
      </div>
      <p className="w-full  h-0.5 rounded-md   bg-gray-200" > </p>
@@ -30,13 +50,20 @@ const ProfileSkeleton = () => {
         <p className="h-3 w-6 rounded bg-gray-200  animate-pulse"> </p>
         </div>
 
-        <main className="grid grid-cols-3 mt-10 gap-3">
-            <div className=" bg-gray-200  animate-pulse h-[320px] w-[320px]"> </div>
-            <div className=" bg-gray-200  animate-pulse h-[320px] w-[320px]"> </div>
-            <div className=" bg-gray-200  animate-pulse h-[320px] w-[320px]"> </div>
-            <div className=" bg-gray-200  animate-pulse h-[320px] w-[320px]"> </div>
-            <div className=" bg-gray-200  animate-pulse h-[320px] w-[320px]"> </div>
-            <div className=" bg-gray-200  animate-pulse h-[320px] w-[320px]"> </div>
+        <main className="flex flex-col items-center gap-2 mt-5 sm:mt-7   justify-center " >
+               
+               <div className="flex gap-2">
+               <SkeletonSquare></SkeletonSquare>
+                <SkeletonSquare></SkeletonSquare>
+                <SkeletonSquare></SkeletonSquare>
+               </div>
+
+               <div className="flex gap-2">
+               <SkeletonSquare></SkeletonSquare>
+                <SkeletonSquare></SkeletonSquare>
+                <SkeletonSquare></SkeletonSquare>
+               </div>
+          
 
              </main>
     </div> );
